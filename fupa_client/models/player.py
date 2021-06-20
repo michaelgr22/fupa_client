7fed96e6104cc19a34e5d989ba73fdd4197446eb
michaelgr22 <michael.graml22@outlook.de>
2021-06-20 17:57:00 +0200

    splitted name to firstname and surname

--- fupa_client/models/player.py
+++ fupa_client/models/player.py
@@ -1,7 +1,8 @@
 class Player:
 
-    def __init__(self, name, age, deployments, goals, position, imagelink):
-        self.name = name
+    def __init__(self, firstname, surname, age, deployments, goals, position, imagelink):
+        self.firstname = firstname
+        self.surname = surname
         self.age = int(age) if age else None
         self.deployments = int(deployments) if deployments else None
         self.goals = int(goals) if goals else None
@@ -13,8 +14,11 @@ class Player:
         datasoup = soup.findChildren(
             'a', recursive=False)[0].findChildren('div', recursive=False)[0].findChildren('div', recursive=False)[1]
         spans = datasoup.findAll('span')
+        firstname = spans[0].text.split(' ')[0]
+        surname = spans[0].text.split(' ')[1]
         return cls(
-            name=spans[0].text,
+            firstname=firstname,
+            surname=surname,
             age=spans[1].text.split(' ')[0],
             deployments=spans[2].text.split(' ')[0],
             goals=spans[3].text.split(' ')[0],
@@ -23,5 +27,5 @@ class Player:
         )
 
     def to_dict(self):
-        return {'name': self.name, 'age':  self.age, 'deployments': self.deployments,
+        return {'firstname': self.firstname, 'surname': self.surname, 'age':  self.age, 'deployments': self.deployments,
                 'goals': self.goals, 'position': self.position, 'imagelink': self.imagelink}
